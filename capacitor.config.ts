3dcd60936b420a66772b632e0c4097be907d6a6a
Developer <dev@example.com>
2026-01-23 18:09:23 -0800

    fix: Remove static export config to support API routes for Vercel

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.xuenzf.rentapp',
   appName: '租房APP',
-  webDir: 'out',
+  webDir: '.next',
   server: {
     androidScheme: 'https'
   },
