3be1b87687691f6700fe51fdebe0acca49692181
lix771859-ux <lix771859@gmail.com>
2026-02-02 14:33:49 -0800

    1

--- src/app/landlord/properties/page.tsx
+++ src/app/landlord/properties/page.tsx
@@ -203,8 +203,12 @@ const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
       setForm({ title: '', price: 0, image: null, video: null });
       // 新增后首页刷新
       // useRefreshStore.getState().markShouldRefresh();
-      mutate('/api/properties?page=1&pageSize=6&minPrice=1000&maxPrice=15000')
-
+      // mutate('/api/properties?page=1&pageSize=6&minPrice=1000&maxPrice=15000')
+      fetch(`/api/properties?page=1&pageSize=6&minPrice=1000&maxPrice=15000`)
+      .then((r) => r.json())
+      .then((res) => setProperties(res.data ?? []))
+      .catch(() => {});
+      
       router.push('/');
     } catch (err: any) {
       setMessage(err.message || '发布失败');
