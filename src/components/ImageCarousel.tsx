dd3d7850e8d85ab31b4e9266054c04d27df60073
lix771859-ux <lix771859@gmail.com>
2026-02-02 19:48:17 -0800

    1

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -91,7 +91,7 @@ export default function ImageCarousel({ images, title = '', className = '' }: Im
 
           {/* 顶部计数器 */}
           <div onClick={(e) => {e.stopPropagation();}}
- className="absolute top-3 right-3  bg-black/50 text-white px-3 py-1 rounded-full text-sm z-10">
+ className="absolute bottom-3 right-3  bg-black/50 text-white px-3 py-1 rounded-full text-sm z-10">
             {currentIndex + 1} / {images.length}
           </div>
 
